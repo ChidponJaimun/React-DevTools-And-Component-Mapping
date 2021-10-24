f8f281f81c8d4923101a7ab65c30c7c521045ff7
ChidponJaimun <61172207+ChidponJaimun@users.noreply.github.com>
2021-10-24 16:22:59 +0700

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,25 @@ import React from "react";
 import Card from "./Card";
 import contacts from "../contacts";
 import Avatar from "./Avatar";
+
+function createCard(contacts) {
+  return (
+    <Card
+      name={contacts.name}
+      img={contacts.imgURL}
+      tel={contacts.phone}
+      email={contacts.email}
+    />
+  );
+}
+
 function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
       <Avatar img="https://i.pinimg.com/originals/4a/e9/4d/4ae94df989d0cb748bc5a98c0d692ee3.jpg" />
-      <Card
+      {contacts.map(createCard)}
+      {/* <Card
         name={contacts[0].name}
         img={contacts[0].imgURL}
         tel={contacts[0].phone}
@@ -24,7 +37,7 @@ function App() {
         img={contacts[2].imgURL}
         tel={contacts[2].phone}
         email={contacts[2].email}
-      />
+      /> */}
     </div>
   );
 }
